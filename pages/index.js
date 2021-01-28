6f9155b5c2c57c7de98f5f73f16741360515f57e
Surf <guille.theoffspring@gmail.com>
2021-01-28 13:54:41 -0300

    Modified home page

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,12 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <span>Hello World!!</span>
+    <div className="page-container">
+      <div className={styles.main}>
+        <h1>Next.js News App</h1>
+
+        <h3>Your one stop shop for the latest news articles</h3>
+      </div>
+    </div>
   )
 }
